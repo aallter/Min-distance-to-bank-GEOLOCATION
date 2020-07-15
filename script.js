dd5a0ded6c78cb65138f5b318907373a5e10c1e6
pavell_chip <pavellchip@gmail.com>
2020-07-15 23:00:33 +0300

    Working functions , showing min distance in console.log.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /*Url api bankomats*/
 let url = 'http://courses.dp.ua/api/pb_atm.php';
 
+
+// console.log(lat_user);
 (async function(){
 
   /*get massive*/
@@ -11,73 +13,80 @@ let url = 'http://courses.dp.ua/api/pb_atm.php';
   result     = result.devices;
 
   /*new mass */
-  let arr_bankomats;
-  (function(){
-      arr_bankomats = result.map(item=>({
+      let arr_bankomats = result.map(item=>({
       city:item.cityRU,
       addres:item.fullAddressRu,
       latitude_bank : item.latitude,
       longitude_bank : item.longitude
     }));
-    return arr_bankomats;
-  }());
 
-  /*Geolocation ME*/
-var lat_user,lng_user,lat_bank,lng_bank;
-var arr_d=[];
+    
+ 
+       /*Formula Goversina*/
+let Dist = () =>{
+  let arr_d=[];
+  for(let i=0;i<arr_bankomats.length;i++){
+    let lng_bank=arr_bankomats[i].latitude_bank;
+    let lat_bank =arr_bankomats[i].longitude_bank;
+    var _lat1 = window.lat;
+    var _lon1 =  window.lng;
+    function _getDistanceFromLatLonInKm(_lat1, _lon1, lat_bank, lng_bank) {
+              var R = 6371; // Radius of the earth in kilometers
+              var dLat = deg2rad(lat_bank - window.lat); // deg2rad below
+              var dLon = deg2rad(lng_bank - window.lng);
+              var a =
+                Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(deg2rad(window.lat)) * Math.cos(deg2rad(lat_bank)) *
+                Math.sin(dLon / 2) * Math.sin(dLon / 2);
+              var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+              var d = R * c; // Distance in KM
+              return d;
+        };
 
-/*Body script U*/
-   for(let i=0; i <=8;i++){
-   navigator.geolocation.getCurrentPosition((pos)=>{
-    console.log("OK",pos);
-      lat_user=pos.coords.latitude;
-      lng_user=pos.coords.longitude;
-     /*Formula Goversina*/
-     /*Cicle */
+    function deg2rad(deg) {
+              return deg * (Math.PI / 180)
+        }
 
-       /*BANK lat lng*/
-       lng_bank=arr_bankomats[i].latitude_bank;
-       lat_bank =arr_bankomats[i].longitude_bank;
-       /*end bank*/
+        // Point 1: 15.008809, 78.659096
+        // Point 2: 13.90457539, 78.5855514
 
-       function _getDistanceFromLatLonInKm(lat_user, lng_user, lat_bank, lng_bank) {
-            var R = 6371; // Radius of the earth in kilometers
-            var dLat = deg2rad(lat_bank - lat_user); // deg2rad below
-            var dLon = deg2rad(lng_bank - lng_user);
-            var a =
-              Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-              Math.cos(deg2rad(lat_user)) * Math.cos(deg2rad(lat_bank)) *
-              Math.sin(dLon / 2) * Math.sin(dLon / 2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            var d = R * c; // Distance in KM
-            return d;
-       }
 
-       function deg2rad(deg) {
-            return deg * (Math.PI / 180)
-       }
+    var _lat2 = lat_bank;
+    var _lon2 = lng_bank;
 
-       // Point 1: 15.008809, 78.659096
-       // Point 2: 13.90457539, 78.5855514
-       //
-       var _lat1 = lat_user;
-       var _lon1 = lng_user;
+        // precise value
+    var _d = _getDistanceFromLatLonInKm(_lat1,_lon1 ,_lat2 ,_lon2 );
+    // alert(_d);
+        // round value
+  _d =  Math.round(_getDistanceFromLatLonInKm(_lat1,_lon1 ,_lat2 ,_lon2 ) * 100) / 100 ;
+  // alert(_d);
+    arr_d.push({distance:_d,addres:arr_bankomats[i].addres}); 
+    
+  };
+  let min_dis_to_bank = arr_d.reduce((acc, curr) => acc.distance < curr.distance ? acc : curr);
+  console.log(min_dis_to_bank);
+  console.log("Самый близкий банкомат по адрессу: ",min_dis_to_bank.addres);
+  console.log("До него km: ",min_dis_to_bank.distance);
+      } 
 
-       var _lat2 = lat_bank;
-       var _lon2 = lng_bank;
+/*Geolation Me 2.0*/ 
 
-       // precise value
-       var _d = "Precise value: " + _getDistanceFromLatLonInKm(_lat1,_lon1 ,_lat2 ,_lon2 );
-       console.log(_d); // alert(_d);
-       // round value
-       _d = "Round value: " +
-            Math.round(_getDistanceFromLatLonInKm(_lat1,_lon1 ,_lat2 ,_lon2 ) * 100) / 100 +
-            " km";
-       console.log(i,arr_bankomats[i].addres,_d); // alert(_d);
-        arr_d.push(_d);
-      /*End cicle*/
-    });
-};
-console.log(arr_d);
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      document.body.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
 
+  function showPosition(position) {
+    document.body.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+    window.lat = position.coords.latitude;
+    window.lng = position.coords.longitude;
+    Dist();
+  }
+
+  getLocation();
+ 
 })();
