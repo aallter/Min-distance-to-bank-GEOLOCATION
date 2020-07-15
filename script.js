7b187f1ddbd74c9af58df3c45554fedbb6b92765
pavell_chip <pavellchip@gmail.com>
2020-07-15 23:16:24 +0300

    Working code. Show result on web-page , all comments in code on places,good visual code

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 /*Url api bankomats*/
 let url = 'http://courses.dp.ua/api/pb_atm.php';
 
-
-// console.log(lat_user);
 (async function(){
 
-  /*get massive*/
+//get massive
   let result = await fetch(url);
   result     = await result.json();
 
-  /*get massive with in devices*/
+//get massive with in devices
   result     = result.devices;
 
-  /*new mass */
+//new mass 
       let arr_bankomats = result.map(item=>({
       city:item.cityRU,
       addres:item.fullAddressRu,
       latitude_bank : item.latitude,
       longitude_bank : item.longitude
     }));
-
-    
- 
-       /*Formula Goversina*/
+//Formula Goversina
 let Dist = () =>{
   let arr_d=[];
   for(let i=0;i<arr_bankomats.length;i++){
@@ -42,34 +37,29 @@ let Dist = () =>{
               var d = R * c; // Distance in KM
               return d;
         };
-
     function deg2rad(deg) {
               return deg * (Math.PI / 180)
         }
-
-        // Point 1: 15.008809, 78.659096
-        // Point 2: 13.90457539, 78.5855514
-
-
     var _lat2 = lat_bank;
     var _lon2 = lng_bank;
-
-        // precise value
+// precise value
     var _d = _getDistanceFromLatLonInKm(_lat1,_lon1 ,_lat2 ,_lon2 );
-    // alert(_d);
-        // round value
+// round value
   _d =  Math.round(_getDistanceFromLatLonInKm(_lat1,_lon1 ,_lat2 ,_lon2 ) * 100) / 100 ;
-  // alert(_d);
+// push array
     arr_d.push({distance:_d,addres:arr_bankomats[i].addres}); 
-    
   };
   let min_dis_to_bank = arr_d.reduce((acc, curr) => acc.distance < curr.distance ? acc : curr);
+//Show results code
   console.log(min_dis_to_bank);
   console.log("Самый близкий банкомат по адрессу: ",min_dis_to_bank.addres);
   console.log("До него km: ",min_dis_to_bank.distance);
+      
+  document.body.innerHTML = "Самый близкий банкомат по адрессу:  " + min_dis_to_bank.addres + 
+  "<br>До него km: " + min_dis_to_bank.distance;
       } 
 
-/*Geolation Me 2.0*/ 
+/*Two main function */ 
 
   function getLocation() {
     if (navigator.geolocation) {
@@ -86,7 +76,6 @@ let Dist = () =>{
     window.lng = position.coords.longitude;
     Dist();
   }
-
+  //Main 
   getLocation();
- 
 })();
